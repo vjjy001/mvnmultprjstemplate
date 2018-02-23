e95db9b636338aac27e7c7d69ec43d37a1027baa
James <James@James>
2018-02-22 19:13:51 -0500

    commit for maven web app multiple projects

---
+++ LAUSCore/src/main/java/com/bls/fsms/App.java
@@ -0,0 +1,13 @@
+package com.bls.fsms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
